c4e19875a7b3aa6428eb09a9d942d225e792e710
Ls7sq <lsq9388@gmail.com>
2021-07-04 16:13:11 +0800

    Docker

---
+++ oracleTest/src/test/java/sg/shiqi/test/oracleTest/OracleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.shiqi.test.oracleTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
